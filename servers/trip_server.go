6e2c1bebae72d6f3b1055fb6678d349447e01023
Ron DeMeritt <ron.demeritt@coinbase.com>
2023-09-29 18:28:23 -0400

    finially working

--- servers/trip_server.go
+++ servers/trip_server.go
@@ -1,24 +1,25 @@
 package servers
 
 import (
-    "net"
-    "rideshare/common"
-    "rideshare/gmapsclient"
-	t "rideshare/trip"
-    trippb "rideshare/proto/trip"
-    log "github.com/sirupsen/logrus"
+	"context"
+	"net"
+	"rideshare/common"
+	"rideshare/gmapsclient"
+	trippb "rideshare/proto/trip"
+	trip "rideshare/trip"
+	"time"
 
-    "google.golang.org/grpc"
+	log "github.com/sirupsen/logrus"
+
+	"google.golang.org/grpc"
 )
 
-type TripServer struct{}
-
-func NewTripServer() *TripServer {
-    return &TripServer{}
+type server struct {
+    trippb.TripServiceServer
 }
 
-func (s *TripServer) StartTripServer(port string) {
-    // Listen on the specified gRPC port
+func StartTripServer(port string) {
+    // Listen on the specified port
     lis, err := net.Listen("tcp", ":" + port)
     if err != nil {
         log.Fatalf("failed to listen: %v", err)
@@ -26,10 +27,10 @@ func (s *TripServer) StartTripServer(port string) {
 
     // Create a new gRPC server
     grpcServer := grpc.NewServer()
-    var tripServiceServer trippb.TripServiceServer
+    // var tripServiceServer trippb.TripServiceServer
 
     // Register the TripService server
-    trippb.RegisterTripServiceServer(grpcServer, tripServiceServer)
+    trippb.RegisterTripServiceServer(grpcServer, &server{})
 
     // Start the gRPC server
     log.Debugf("Starting gRPC server on port %s", port)
@@ -38,24 +39,31 @@ func (s *TripServer) StartTripServer(port string) {
     }
 }
 
-func (s *TripServer) CalculateNewTrip(req *trippb.TripRequest) *trippb.TripResponse {
+// create a dummy service to return today's date and time in nyc
+func (s *server) GetTimeInNYC(ctx context.Context, _ *trippb.NoInput) (*trippb.StringResponse, error) {
+    log.Debugf("GetTime request")
+    return &trippb.StringResponse{Value: time.Now().Local().String()}, nil
+}
+
+func (s *server) CalculateNewTrip(ctx context.Context, req *trippb.TripRequest) (*trippb.TripResponse, error) {
+    log.Debugf("CalculateNewTrip request: %v", req)
     // Create a new Trip object
-    trip := t.NewTrip(req.PassengerStart, req.PassengerEnd, req.DriverLocation, req.DistanceUnits)
+    t := trip.NewTrip(req.PassengerStart, req.PassengerEnd, req.DriverLocation, req.DistanceUnits)
 
     // Create a new maps client
 	client, err := gmapsclient.NewMapsClient()
 	common.Check(err)
 
-    dmr, err := t.GetDistanceMatrix(client, trip.Coordinates.DriverLocation, trip.Coordinates.PassengerStart, trip.Coordinates.PassengerEnd, trip.Units.Distance)
+    dmr, err := trip.GetDistanceMatrix(client, t.Coordinates.DriverLocation, t.Coordinates.PassengerStart, t.Coordinates.PassengerEnd, t.Units.Distance)
     common.Check(err)
 
-    trip.PopulateTripDetails(dmr)
+    t.PopulateTripDetails(dmr)
 
     // Create a new TripResponse object
     return &trippb.TripResponse{
-        PassengerStartToPassengerEndDistance: trip.Details.PassengerStartToPassengerEndDistance,
-        PassengerStartToPassengerEndDuration: trip.Details.PassengerStartToPassengerEndDuration.String(),
-        DriverLocationToPassengerStartDistance: trip.Details.DriverLocationToPassengerStartDistance,
-        DriverLocationToPassengerStartDuration: trip.Details.DriverLocationToPassengerStartDuration.String(),
-    }
+        PassengerStartToPassengerEndDistance: t.Details.PassengerStartToPassengerEndDistance,
+        PassengerStartToPassengerEndDuration: t.Details.PassengerStartToPassengerEndDuration.String(),
+        DriverLocationToPassengerStartDistance: t.Details.DriverLocationToPassengerStartDistance,
+        DriverLocationToPassengerStartDuration: t.Details.DriverLocationToPassengerStartDuration.String(),
+    }, nil
 }
