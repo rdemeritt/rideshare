1c7be5d9ee2cfdfedcb2e0340e126204264dee95
Ron DeMeritt <ron.demeritt@coinbase.com>
2023-09-29 20:25:50 -0400

    working

--- servers/trip_server.go
+++ servers/trip_server.go
@@ -15,55 +15,54 @@ import (
 )
 
 type server struct {
-    trippb.TripServiceServer
+	trippb.TripServiceServer
 }
 
 func StartTripServer(port string) {
-    // Listen on the specified port
-    lis, err := net.Listen("tcp", ":" + port)
-    if err != nil {
-        log.Fatalf("failed to listen: %v", err)
-    }
+	// Listen on the specified port
+	lis, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
 
-    // Create a new gRPC server
-    grpcServer := grpc.NewServer()
-    // var tripServiceServer trippb.TripServiceServer
+	// Create a new gRPC server
+	grpcServer := grpc.NewServer()
 
-    // Register the TripService server
-    trippb.RegisterTripServiceServer(grpcServer, &server{})
+	// Register the TripService server
+	trippb.RegisterTripServiceServer(grpcServer, &server{})
 
-    // Start the gRPC server
-    log.Debugf("Starting gRPC server on port %s", port)
-    if err := grpcServer.Serve(lis); err != nil {
-        log.Fatalf("failed to serve: %v", err)
-    }
+	// Start the gRPC server
+	log.Debugf("Starting gRPC server on port %s", port)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
 
 // create a dummy service to return today's date and time in nyc
 func (s *server) GetTimeInNYC(ctx context.Context, _ *trippb.NoInput) (*trippb.StringResponse, error) {
-    log.Debugf("GetTime request")
-    return &trippb.StringResponse{Value: time.Now().Local().String()}, nil
+	log.Debugf("GetTime request")
+	return &trippb.StringResponse{Value: time.Now().Local().String()}, nil
 }
 
 func (s *server) CalculateNewTrip(ctx context.Context, req *trippb.TripRequest) (*trippb.TripResponse, error) {
-    log.Debugf("CalculateNewTrip request: %v", req)
-    // Create a new Trip object
-    t := trip.NewTrip(req.PassengerStart, req.PassengerEnd, req.DriverLocation, req.DistanceUnits)
+	log.Debugf("CalculateNewTrip request: %v", req)
+	// Create a new Trip object
+	t := trip.NewTrip(req.PassengerStart, req.PassengerEnd, req.DriverLocation, req.DistanceUnits)
 
-    // Create a new maps client
+	// Create a new maps client
 	client, err := gmapsclient.NewMapsClient()
 	common.Check(err)
 
-    dmr, err := trip.GetDistanceMatrix(client, t.Coordinates.DriverLocation, t.Coordinates.PassengerStart, t.Coordinates.PassengerEnd, t.Units.Distance)
-    common.Check(err)
+	dmr, err := trip.GetTripRequestDistanceMatrix(client, req)
+	common.Check(err)
 
-    t.PopulateTripDetails(dmr)
+	t.PopulateTripDetails(dmr)
 
-    // Create a new TripResponse object
-    return &trippb.TripResponse{
-        PassengerStartToPassengerEndDistance: t.Details.PassengerStartToPassengerEndDistance,
-        PassengerStartToPassengerEndDuration: t.Details.PassengerStartToPassengerEndDuration.String(),
-        DriverLocationToPassengerStartDistance: t.Details.DriverLocationToPassengerStartDistance,
-        DriverLocationToPassengerStartDuration: t.Details.DriverLocationToPassengerStartDuration.String(),
-    }, nil
+	// Create a new TripResponse object
+	return &trippb.TripResponse{
+		PassengerStartToPassengerEndDistance:   t.Details.PassengerStartToPassengerEndDistance,
+		PassengerStartToPassengerEndDuration:   t.Details.PassengerStartToPassengerEndDuration.String(),
+		DriverLocationToPassengerStartDistance: t.Details.DriverLocationToPassengerStartDistance,
+		DriverLocationToPassengerStartDuration: t.Details.DriverLocationToPassengerStartDuration.String(),
+	}, nil
 }
--- trip/directions.go
+++ trip/directions.go
@@ -4,53 +4,73 @@ import (
 	"context"
 	"fmt"
 
-	"googlemaps.github.io/maps"
+	trippb "rideshare/proto/trip"
+
 	log "github.com/sirupsen/logrus"
+	"googlemaps.github.io/maps"
 )
 
 func PrintDirections(directions []maps.Route) {
 	// Print the driving directions
-    for _, route := range directions {
-        for _, leg := range route.Legs {
-            for _, step := range leg.Steps {
-                fmt.Println(step.HTMLInstructions)
-            }
-        }
-    }
+	for _, route := range directions {
+		for _, leg := range route.Legs {
+			for _, step := range leg.Steps {
+				fmt.Println(step.HTMLInstructions)
+			}
+		}
+	}
 }
 
 func GetDirections(client *maps.Client, start string, end string) ([]maps.Route, error) {
-	 // Build the directions request
-	 	directionsRequest := &maps.DirectionsRequest{
-        Origin:      start,
-        Destination: end,
-    }
+	// Build the directions request
+	directionsRequest := &maps.DirectionsRequest{
+		Origin:      start,
+		Destination: end,
+	}
 
 	// Send the directions request
-    routes, _, err := client.Directions(context.Background(), directionsRequest)
-    if err != nil {
-        return nil, err
-    }
+	routes, _, err := client.Directions(context.Background(), directionsRequest)
+	if err != nil {
+		return nil, err
+	}
 
 	return routes, nil
 }
 
 func GetDistanceMatrix(client *maps.Client, driverLocation string, passengerStart string, passengerEnd string, units string) (*maps.DistanceMatrixResponse, error) {
-    // Build the distance matrix request
-    request := &maps.DistanceMatrixRequest{
-        Origins:      []string{driverLocation, passengerStart},
-        Destinations: []string{passengerStart, passengerEnd},
-    }
+	// Build the distance matrix request
+	request := &maps.DistanceMatrixRequest{
+		Origins:      []string{driverLocation, passengerStart},
+		Destinations: []string{passengerStart, passengerEnd},
+	}
 
 	SetUnits(units, request)
 
-    // Send the distance matrix request
-    response, err := client.DistanceMatrix(context.Background(), request)
-    if err != nil {
-        return nil, err
-    }
+	// Send the distance matrix request
+	response, err := client.DistanceMatrix(context.Background(), request)
+	if err != nil {
+		return nil, err
+	}
 
-    return response, nil
+	return response, nil
+}
+
+func GetTripRequestDistanceMatrix(client *maps.Client, req *trippb.TripRequest) (*maps.DistanceMatrixResponse, error) {
+	// Build the distance matrix request
+	request := &maps.DistanceMatrixRequest{
+		Origins:      []string{req.DriverLocation, req.PassengerStart},
+		Destinations: []string{req.PassengerStart, req.PassengerEnd},
+	}
+
+	SetUnits(req.DistanceUnits, request)
+
+	// Send the distance matrix request
+	response, err := client.DistanceMatrix(context.Background(), request)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
 }
 
 func SetUnits(units string, r *maps.DistanceMatrixRequest) {
@@ -68,38 +88,38 @@ func SetUnits(units string, r *maps.DistanceMatrixRequest) {
 
 func PrintFullDistanceMatrix(resp *maps.DistanceMatrixResponse) {
 	// Print the distance matrix
-    for _, row := range resp.Rows {
+	for _, row := range resp.Rows {
 		fmt.Println("row: ", row)
-        for _, element := range row.Elements {
-            if element.Status == "OK" {
-                log.Debugf("Distance: %v\n", element.Distance.HumanReadable)
-                log.Debugf("Duration: %v\n", element.Duration)
-            } else {
-                log.Debugf("Error: %v\n", element.Status)
-            }
-        }
-    }
+		for _, element := range row.Elements {
+			if element.Status == "OK" {
+				log.Debugf("Distance: %v\n", element.Distance.HumanReadable)
+				log.Debugf("Duration: %v\n", element.Duration)
+			} else {
+				log.Debugf("Error: %v\n", element.Status)
+			}
+		}
+	}
 }
 
 func PrintDistanceMatrix(resp *maps.DistanceMatrixResponse) {
-    // Print the distance and duration for the first and last elements
-    if len(resp.Rows) > 0 && len(resp.Rows[0].Elements) > 0 {
-        // Print the distance and duration for the first element
-        element := resp.Rows[0].Elements[0]
-        if element.Status == "OK" {
-            log.Debugf("Distance for first element: %v\n", element.Distance.HumanReadable)
-            log.Debugf("Duration for first element: %v\n", element.Duration)
-        } else {
-            log.Debugf("Error for first element: %v\n", element.Status)
-        }
+	// Print the distance and duration for the first and last elements
+	if len(resp.Rows) > 0 && len(resp.Rows[0].Elements) > 0 {
+		// Print the distance and duration for the first element
+		element := resp.Rows[0].Elements[0]
+		if element.Status == "OK" {
+			log.Debugf("Distance for first element: %v\n", element.Distance.HumanReadable)
+			log.Debugf("Duration for first element: %v\n", element.Duration)
+		} else {
+			log.Debugf("Error for first element: %v\n", element.Status)
+		}
 
-        // Print the distance and duration for the last element
-        element = resp.Rows[len(resp.Rows)-1].Elements[len(resp.Rows[0].Elements)-1]
-        if element.Status == "OK" {
-            log.Debugf("Distance for last element: %v\n", element.Distance.HumanReadable)
-            log.Debugf("Duration for last element: %v\n", element.Duration)
-        } else {
-            log.Debugf("Error for last element: %v\n", element.Status)
-        }
-    }
+		// Print the distance and duration for the last element
+		element = resp.Rows[len(resp.Rows)-1].Elements[len(resp.Rows[0].Elements)-1]
+		if element.Status == "OK" {
+			log.Debugf("Distance for last element: %v\n", element.Distance.HumanReadable)
+			log.Debugf("Duration for last element: %v\n", element.Duration)
+		} else {
+			log.Debugf("Error for last element: %v\n", element.Status)
+		}
+	}
 }
