ef50b082492b4f25211160b55b4c26393c19812a
Ron DeMeritt <ron.demeritt@coinbase.com>
2023-09-29 20:25:50 -0400

    working

---
+++ Makefile
@@ -0,0 +1,14 @@
+.PHONY: protoc
+
+all: protoc
+
+trip_proto: proto/trip.proto
+	protoc --go_out=./proto --go-grpc_out=./proto proto/trip.proto
+
+protoc: trip_proto
+
+run_trip_server:
+	go run cmd/main.go -log-level debug -port 8080
+
+clean:
+	find proto -name "*.pb.go" -type f -delete
--- trip/trip.go
+++ trip/trip.go
@@ -1,34 +1,45 @@
 package trip
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 
+	log "github.com/sirupsen/logrus"
 	"googlemaps.github.io/maps"
 )
 
 
 type Coordinates struct {
-	PassengerStart string
-	PassengerEnd   string
-	DriverLocation string
+	PassengerStart string `json:"passenger_start"`
+    PassengerEnd   string `json:"passenger_end"`
+    DriverLocation string `json:"driver_location"`
 }
 
 type Units struct {
-	Distance string
+	Distance string `json:"distance"`
 }
 
 type Details struct {
-	DriverLocationToPassengerStartDistance string
-	DriverLocationToPassengerStartDuration time.Duration
-	PassengerStartToPassengerEndDistance   string
-	PassengerStartToPassengerEndDuration   time.Duration
+	DriverLocationToPassengerStartDistance string `json:"driver_location_to_passenger_start_distance"`
+    DriverLocationToPassengerStartDuration time.Duration `json:"driver_location_to_passenger_start_duration"`
+    PassengerStartToPassengerEndDistance   string `json:"passenger_start_to_passenger_end_distance"`
+    PassengerStartToPassengerEndDuration   time.Duration `json:"passenger_start_to_passenger_end_duration"`
 }
 
 type Trip struct {
-	Coordinates Coordinates
-	Units       Units
-	Details     Details
+	Coordinates Coordinates `json:"coordinates"`
+	Units       Units `json:"units"`
+	Details     Details `json:"details"`
+}
+
+// convert Trip object to JSON
+func (t *Trip) ToJSON() (string, error) {
+    jsonBytes, err := json.Marshal(t)
+    if err != nil {
+        return "", err
+    }
+    return string(jsonBytes), nil
 }
 
 // create Trip object w/ the Coordinates struct populated
@@ -47,6 +58,7 @@ func NewEmptyTrip() *Trip {
 }
 
 func (t *Trip) PopulateTripDetails(dmr *maps.DistanceMatrixResponse) {
+	log.Debugf("Populating trip details")
 	t.Details.DriverLocationToPassengerStartDistance = dmr.Rows[0].Elements[0].Distance.HumanReadable
 	t.Details.DriverLocationToPassengerStartDuration = dmr.Rows[0].Elements[0].Duration
 	t.Details.PassengerStartToPassengerEndDistance = dmr.Rows[1].Elements[1].Distance.HumanReadable
