3cf8f93a628b558800132b0905adf8f9485fa3ff
Ron DeMeritt <ron.demeritt@coinbase.com>
2023-10-02 13:18:36 -0400

    mostly working

--- trip/distance.go
+++ trip/distance.go
@@ -3,9 +3,14 @@ package trip
 import (
 	"context"
 	"fmt"
+	"rideshare/database"
+	_ "rideshare/gmapsclient"
 	trippb "rideshare/proto/trip"
+	"time"
 
 	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"googlemaps.github.io/maps"
 )
 
@@ -49,6 +54,57 @@ func GetTripRequestDistanceMatrix(client *maps.Client, req *trippb.TripRequest) 
 	}
 
 	return response, nil
+}
+
+// query mongodb for trips that are in pending and within the specified proximity
+func GetTripsInProximity(client *mongo.Client, driver_location string, distance string) {
+	log.Info("GetTripsInProximity start")
+	defer log.Info("GetTripsInProximity end")
+
+	// query mongodb for trips that are in pending
+	collection := client.Database("rideshare").Collection("trips")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// query mongodb for trips that are in pending
+	filter := bson.M{"status": "pending"}
+	cursor, err := collection.Find(ctx, filter)
+	if err != nil {
+		log.Errorf("failed to query MongoDB: %v", err)
+	}
+	// Build the distance matrix request and set Origins as the driver_location
+	request := &maps.DistanceMatrixRequest{Origins: []string{driver_location}}
+
+	var results []bson.M
+	if err = cursor.All(ctx, &results); err != nil {
+		log.Errorf("failed to decode MongoDB cursor: %v", err)
+	}
+
+	// iterate through the results
+	for _, result := range results {
+		// log the result
+		resultJson, _ := database.BsonMToJson(result)
+		log.Debugf("GetTripsInProximity result: %v", result)
+		log.Debugf("GetTripsInProximity resultJson: %s", string(resultJson))
+
+		// set Destinations as the passenger_start
+		request.Destinations = append(request.Destinations, string(resultJson))
+	}
+
+	log.Debugf("GetTripsInProximity request: %v", request)
+
+
+	// // get google maps client
+	// gmapsClient, err = gmapsclient.NewMapsClient()
+	// if err != nil {
+	// 	log.Errorf("failed to create google maps client: %v", err)
+	// }
+
+	// // Send the distance matrix request
+	// response, err := client.DistanceMatrix(context.Background(), request)
+	// if err != nil {
+	// 	log.Errorf("failed to get distance matrix: %v", err)
+	// }
 }
 
 func SetUnits(units string, r *maps.DistanceMatrixRequest) {
