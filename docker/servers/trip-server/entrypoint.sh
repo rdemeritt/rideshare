44e625b3ebcfbc8f49f5fcf80ec0796c29565efa
Ron DeMeritt <ron.demeritt@coinbase.com>
2023-10-23 11:29:53 -0400

    more

--- docker/servers/trip-server/entrypoint.sh
+++ docker/servers/trip-server/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 eval $(cat /app/gmapsapikey | sed 's/^/export GMAPS_API_KEY=/')
-echo $GMAPS_API_KEY
+
 if [ -n "$GMAPS_API_KEY" ]; then
     echo "GMAPS_API_KEY is set, removing secrets file"
+    rm -f /app/gmapsapikey
 else
     echo "GMAPS_API_KEY is not set"
 fi
