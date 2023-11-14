54bf2b04c9cef736785a754a3c01e566e8d319e9
Ron DeMeritt <ron.demeritt@coinbase.com>
2023-11-13 19:35:32 -0500

    more

--- tools/init_mongo.go
+++ tools/init_mongo.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
 	"flag"
+	"os/exec"
+	"math/rand"
 	"rideshare/common"
 	"rideshare/config"
 	"rideshare/database"
@@ -92,4 +95,105 @@ func CancelTripsWithID(client *mongo.Client) error {
 	defer log.Info("CancelTripsWithNonNullID end")
 
 	return nil
+}
+
+
+type City struct {
+	Name    string
+	ZipCode string
+}
+
+type CityCo struct {
+	Cities []City
+}
+
+func PopulateDB(client *mongo.Client) {
+    // build a object containing 10 cities from Fulton County
+    fultonCo := CityCo{
+        Cities: generateFultonCoCities(),
+    }
+
+	// build a object containing 10 cities from Cobb County
+	cobbCo := CityCo{
+		Cities: generateCobbCoCities(),
+	}
+
+	// use grpcurl to insert the cities into the database by calling CreateTripRequest
+	// grpcurl -proto ../proto/*.proto -plaintext -d '{"passenger_start": "Atlanta, 30303", "passenger_end": "Marietta, 30060"}' $(minikube ip):30080 rideshare.TripService/CreateTripRequest
+	// itterate through fulltonCo and cobbCo and mix the cities together
+	// use grpcurl to insert the cities into the database by calling CreateTripRequest
+	// grpcurl -proto ../proto/*.proto -plaintext -d '{"passenger_start": "Atlanta, 30303", "passenger_end": "Marietta, 30060"}' $(minikube ip):30080 rideshare.TripService/CreateTripRequest
+	for _, citia := range fultonCo.Cities {
+		for _, citib := range cobbCo.Cities {
+			// use grpcurl to insert the cities into the database by calling CreateTripRequest
+			// grpcurl -proto ../proto/*.proto -plaintext -d '{"passenger_start": "Atlanta, 30303", "passenger_end": "Marietta, 30060"}' $(minikube ip):30080 rideshare.TripService/CreateTripRequest
+			// randomly chose citia or citib first
+
+			rand.Seed(time.Now().UnixNano())
+			items := []string{"citia", "citib"}
+			chosenItem := items[rand.Intn(len(items))]
+			cmd := exec.Cmd{}
+			if chosenItem == "citia" {
+				cmd = exec.Command("grpcurl", "-proto ../proto/*.proto", "-plaintext", "-d '{\"passenger_start\": \"Atlanta, 30303\", \"passenger_end\": \"Marietta, 30060\"}'", "$(minikube ip):30080", "rideshare.TripService/CreateTripRequest")
+			} else {
+				cmd = exec.Command("grpcurl", "-proto ../proto/*.proto", "-plaintext", "-d '{\"passenger_start\": \"Marietta, 30060\", \"passenger_end\": \"Atlanta, 30303\"}'", "$(minikube ip):30080", "rideshare.TripService/CreateTripRequest")
+			}
+
+			err := cmd.Run()
+			if err != nil {
+				log.Fatal(err)
+			}
+				}
+			
+			}
+}
+
+// function to randomly mix the source and desitnation cities
+func mixCities(citiesa []City, citiesb []City) []City {
+
+}
+
+func generateFultonCoCities() []City {
+    cities := []City{
+        {Name: "Atlanta", ZipCode: "30303"},
+        {Name: "Roswell", ZipCode: "30075"},
+        {Name: "Alpharetta", ZipCode: "30009"},
+        {Name: "Johns Creek", ZipCode: "30022"},
+        {Name: "Sandy Springs", ZipCode: "30350"},
+        {Name: "Union City", ZipCode: "30291"},
+        {Name: "Milton", ZipCode: "30004"},
+        {Name: "East Point", ZipCode: "30344"},
+        {Name: "College Park", ZipCode: "30337"},
+        {Name: "Fairburn", ZipCode: "30213"},
+    }
+
+    return cities
+}
+
+func generateCobbCoCities() []City {
+	// Marietta, GA - 30060
+	// Smyrna, GA - 30080
+	// Kennesaw, GA - 30144
+	// Acworth, GA - 30101
+	// Powder Springs, GA - 30127
+	// Austell, GA - 30106
+	// Mableton, GA - 30126
+	// Vinings (Unincorporated), GA - 30339
+	// Cumberland (Unincorporated), GA - 30339
+	// Sandy Plains (Unincorporated), GA - 30075
+
+	cities := []City{
+		{Name: "Marietta", ZipCode: "30060"},
+		{Name: "Smyrna", ZipCode: "30080"},
+		{Name: "Kennesaw", ZipCode: "30144"},
+		{Name: "Acworth", ZipCode: "30101"},
+		{Name: "Powder Springs", ZipCode: "30127"},
+		{Name: "Austell", ZipCode: "30106"},
+		{Name: "Mableton", ZipCode: "30126"},
+		{Name: "Vinings", ZipCode: "30339"},
+		{Name: "Cumberland", ZipCode: "30339"},
+		{Name: "Sandy Plains", ZipCode: "30075"},
+	}
+
+	return cities
 }
